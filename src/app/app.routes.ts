35041d94dd35ca741876c83d5a00a6920cbc5971
Peter Albertz <albertzpe@uni-trier.de>
2024-11-24 19:28:35 +0100

    refactor: remove feature and about modules
    - Delete feature and about module directories
    - Simplify routing to only include home page
    - Remove lazy loading configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,16 +13,6 @@ export const routes: Routes = [
     title: 'Home'
   },
   {
-    path: 'features',
-    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule),
-    title: 'Features'
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
-    title: 'About'
-  },
-  {
     path: '**',
     redirectTo: 'home'
   }
--- src/app/features/features.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { FeaturesComponent } from './features.component';
-
-export const FEATURES_ROUTES: Routes = [
-  {
-    path: '',
-    component: FeaturesComponent
-  }
-];
